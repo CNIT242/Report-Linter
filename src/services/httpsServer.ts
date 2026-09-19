6f2f017dc95690129ad6c933e33eaae27ff3d57b
CNIT242 <cnit242@proton.me>
2026-09-19 19:10:44 -0400

    https server .key & .crt mixup

--- src/services/httpsServer.ts
+++ src/services/httpsServer.ts
@@ -7,24 +7,23 @@ import api from "./expressServer.js";
 console.log("server");
 
 const options = {
-  key: fs.readFileSync(process.env.SSL_CRT || "./.crt"),
-  cert: fs.readFileSync(process.env.SSL_KEY || "./.key"),
+  cert: fs.readFileSync(process.env.SSL_CRT || "./.crt"),
+  key: fs.readFileSync(process.env.SSL_KEY || "./.key"),
 };
+const server = https.createServer(options,api);
 
-const socketServer = https.createServer(options,api);
-
-socketServer.on("error", (req, res) => { 
+server.on("error", (req, res) => { 
   log.message("error"); 
 });
 
-socketServer.on("secureConnection", (req, res) => {
+server.on("secureConnection", (req, res) => {
     log.message("connection"); 
 });
 
-socketServer.listen(process.env.PORT, () => {
+server.listen(process.env.PORT, () => {
   log.message("listing on port " + process.env.PORT);
 });
 
 
 //EXPORTS
-export default socketServer;
+export default server;
