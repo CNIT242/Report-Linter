e04a97ea5594ffdff35c15ae9326b1e11ac0bd89
CNIT242 <cnit242@proton.me>
2026-09-19 22:15:48 -0400

    ./execute.sh: don't run on failed build.

--- execute.sh
+++ execute.sh
@@ -1,4 +1,8 @@
 #! /bin/bash
-npx tsc --build;
-echo "build success. Initalizing nodejs:";
-node --env-file=.env dist/index.js;
+OUT=$(npx tsc --build --pretty);
+printf "%s\n" "$OUT"
+
+if [[ $OUT == "" ]]; then
+    echo "build success. Initalizing nodejs:";
+    node --env-file=.env dist/index.js;
+fi
